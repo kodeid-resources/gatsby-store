e8f8ec0fb09964e220f41592abd2a41c3f3d391f
Riza Fahmi <rizafahmi@gmail.com>
2019-05-14 09:09:41 +0700

    Shop page

---
+++ src/pages/shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+
+const Shop = () => {
+  return (
+    <Layout>
+      <h2>Shop</h2>
+    </Layout>
+  );
+};
+
+export default Shop;
